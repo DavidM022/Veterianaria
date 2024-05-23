f130074d936e65d48a0e720c9763ab701e876e0a
DavidM022 <David.19.mamani@gmail.com>
2024-05-22 21:41:21 -0400

    pruebas unitarias

---
+++ tests/pruebas-unitarias/veterinaria.test.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const request = require('supertest');
+const reservasRutas = require('../../rutas/reservasRutas');
+const RecetaModel = require('../../models/RegistroReserva');
+const mongoose  = require('mongoose');
+const app = express();
+app.use(express.json());
+app.use('/reservas', reservasRutas);
+
+
+describe('Pruebas Unitarias para Veterina', () => {
+
+    //se ejecuta antes de iniciar las pruebas
+    beforeEach(async () => {
+        await mongoose.connect('mongodb://127.0.0.1:27017/veterinaria',{
+            useNewUrlParser : true,            
+        });
+        await RecetaModel.deleteMany({});
+    });
+
+    // al finalziar las pruebasx|
+    afterAll(() => {
+        return mongoose.connection.close();
+      });
+
+    //1er test : GET
+    test('Deberia Traer todas las reservas metodo: GET: ReservasTotal', async() =>{
+        await RecetaModel.create({ ci: 12345678, nombre: 'Wilson Chura', paciente: 'Perro', tipo: 'Cosnulta', fecha: '21-05-2024', hora: '13:00' });
+        await RecetaModel.create({ ci: 13180493, nombre: 'Wilmer Tito', paciente: 'Gato', tipo: 'Cosnulta', fecha: '22-05-2024', hora: '17:00' });
+        // solicitud - request
+        const res =  await request(app).get('/reservas/ReservasTotal');
+        //verificar la respuesta
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveLength(2);
+    }, 10000);
+
+    test('Deberia agregar una nueva Reserva: POST: /crear', async() => {
+        const nuevaReserva = {
+            ci: 13180493, 
+            nombre: 'David Mamani',
+            paciente: 'Perro',
+            tipo: 'consulta', 
+            fecha: '13-05-2024', 
+            hora: '15:00'
+        };
+        const res =  await request(app)
+                            .post('/reservas/crearRegistro')
+                            .send(nuevaReserva);
+        expect(res.statusCode).toEqual(201);
+        expect(res.body.ci).toEqual(nuevaReserva.ci);
+    });
+
+    test('Deberia actualizar una tarea que ya existe: PUT /editar/:id', async()=>{
+        const reservaCreada = await RecetaModel.create({ 
+            ci: 12345678, 
+            nombre: 'Wilson Chura', 
+            paciente: 'Perro', 
+            tipo: 'Cosnulta', 
+            fecha: '21-05-2024', 
+            hora: '13:00' });
+
+            const reservaActualizar = {
+                ci: 12345678, 
+                nombre: 'Wilson Chura (editado)', 
+                paciente: 'Perro (editado)', 
+                tipo: 'Cosnulta', 
+                fecha: '21-05-2024', 
+                hora: '13:00'
+            };
+
+        const res =  await request(app)
+                            .put('/reservas/editarRegistro/'+reservaCreada._id)
+                            .send(reservaActualizar);
+        expect(res.statusCode).toEqual(201);
+        expect(res.body.ci).toEqual(reservaActualizar.ci);                   
+
+    });
+
+    test('Deberia eliminar una tarea existente : DELETE /eliminar/:id', async() =>{
+        
+        const reservaCreada = await RecetaModel.create({ 
+            ci: 12345678, 
+            nombre: 'Wilson Chura', 
+            paciente: 'Perro', 
+            tipo: 'Cosnulta', 
+            fecha: '21-05-2024', 
+            hora: '13:00' });
+
+        const res =  await request(app)
+                                .delete('/reservas/eliminarRegistro/'+reservaCreada._id);
+        
+        expect(res.statusCode).toEqual(200);
+    });
+
+});
